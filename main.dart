2f5962f8cae1306efc10f7717be25d2c1264183e
Salisali Christian L <168385628+squisheiii@users.noreply.github.com>
2025-09-15 05:07:05 +0000

    Building your flutter Image Carousel App

---
+++ main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Profile App',
+      theme:  ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+
+  final List imageUrls = [
+    'https://picsum.photos/400/300?random=1',
+    'https://picsum.photos/400/300?random=2',
+    'https://picsum.photos/400/300?random=3',
+    'https://picsum.photos/400/300?random=4',
+    'https://picsum.photos/400/300?random=5',
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Gallery'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: imageUrls.length,
+                itemBuilder: (context, index){
+                  return Container(
+                    margin: EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        imageUrls[index],
+                        width: 400,
+                        height: 400,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                }
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text('Image Gallery',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),),
+                SizedBox(height: 8),
+                Text('Swipe left or right to explore there amazing photos.',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[600]
+                  ),
+                  textAlign: TextAlign.center,),
+
+              ],
+            ),
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
+
+
